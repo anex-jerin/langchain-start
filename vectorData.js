42ad97ae0b1bc02c2b9ea014e8b3a01922836602
anex jerin <anexcruise@gmail.com>
2023-05-09 18:14:24 +0530

    vector fix

--- vectorData.js
+++ vectorData.js
@@ -2,9 +2,10 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio';
+import { config } from 'dotenv';
+config();
 
-
-
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const loader = new CheerioWebBaseLoader(
   'https://www.india.gov.in/topics/social-development/other-backward-classes',
@@ -15,25 +16,18 @@ const loader = new CheerioWebBaseLoader(
 
 const rawDocs = await loader.load();
 
-
-
 const textSplitter = new RecursiveCharacterTextSplitter({
   chunkSize: 1000,
   chunkOverlap: 200,
 });
 
-const docs = await textSplitter.splitDocuments(rawDocs)
-
-
+const docs = await textSplitter.splitDocuments(rawDocs);
 
 const vectorStore = await MemoryVectorStore.fromDocuments(
   docs,
-  new OpenAIEmbeddings()
+  new OpenAIEmbeddings({
+    openAIApiKey: OPENAI_API_KEY,
+  })
 );
 
-
-
-console.log(vectorStore)
-
-
-
+console.log(vectorStore);
